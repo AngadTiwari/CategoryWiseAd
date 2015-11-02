7cbde6cf55b207a7107839067af84eaff79425e6
angtwr31 <angtwr31@gmail.com>
2015-11-02 11:53:14 +0530

    SOAPService Consume in Android

--- app/src/main/java/com/synsoft/categorywisead/XmlParserActivity.java
+++ app/src/main/java/com/synsoft/categorywisead/XmlParserActivity.java
@@ -1,6 +1,7 @@
 package com.synsoft.categorywisead;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,11 +14,17 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+import org.xml.sax.Parser;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
@@ -36,8 +43,54 @@ public class XmlParserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xml_parser);
 
+        new GetStateListAsync().execute();
+    }
+
+    public boolean getStates()
+    {
+        String METHOD_NAME = "GetStateList"; // our
+        String SOAP_ACTION = "http://emstv.in/Service1.asmx/GetStates";
+
+        String URL = "http://emstv.in/Service1.asmx";
+        String DATABASEURL = "http://touchmee.in/touchmeeservice.asmx";
+        String NAMESPACE = "http://tempuri.org/";
+        String result = "";
+
+        try {
+            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+
+            SoapSerializationEnvelope envelop = new SoapSerializationEnvelope(
+                    SoapEnvelope.VER12);
+            envelop.dotNet = true;
+            envelop.setOutputSoapObject(request);
+            HttpTransportSE aht = new HttpTransportSE(URL);
+            aht.call(SOAP_ACTION, envelop);
+            SoapPrimitive response = (SoapPrimitive) envelop.getResponse();
+            result = response.toString();
+            Log.e("RETURNED RESULT", "" + result);
+            parseSOAPResponse(result,false);
+            return true;
+            /*parser = new Parser(ctx);
+            parser.parseStates(result);*/
+
+        }
+        catch (Exception e)
+        {
+            result = e.toString();
+            Log.e("RETURNED RESULT", "" + result);
+
+            return false;
+        }
+    }
+
+    public void parseSOAPResponse(String xmlString,boolean hasToRemoveXMLTag)
+    {
         ArrayList<HashMap<String, String>> menuItems = new ArrayList<HashMap<String, String>>();
-        String xml = "<string xmlns=\"http://tempuri.org/\">\n" +
+
+        String xml="";
+        if(hasToRemoveXMLTag)
+        {
+            xml = "<string xmlns=\"http://tempuri.org/\">\n" +
                 "<?xml version=\"1.0\"?>" +
                 "<ArrayOfSVideoList xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">" +
                 "<sVideoList>" +
@@ -51,9 +104,10 @@ public class XmlParserActivity extends AppCompatActivity {
                 "</sVideoList><" +
                 "PageNumber>1</PageNumber><TotalVideos>100</TotalVideos></ArrayOfSVideoList>\n" +
                 "</string>"; // getting XML
-        xml=xml.replace("<?xml version=\"1.0\"?>","");
+            xml=xml.replace("<?xml version=\"1.0\"?>","");
+        }
 
-        Document doc = getDomElement(xml); // getting DOM element
+        Document doc = getDomElement(hasToRemoveXMLTag?xml:xmlString); // getting DOM element
 
         NodeList nl = doc.getElementsByTagName("sVideoList");
         // looping through all item nodes <item>
@@ -78,6 +132,27 @@ public class XmlParserActivity extends AppCompatActivity {
                 R.id.textView, R.id.textView2, R.id.textView3 });
 
         ((ListView)findViewById(R.id.list)).setAdapter(adapter);
+    }
+
+    public class GetStateListAsync extends AsyncTask<Void,Void,Boolean>
+    {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            return getStates();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aVoid) {
+
+            if(!aVoid)
+                parseSOAPResponse(null,true);
+        }
     }
 
     public Document getDomElement(String xml){
